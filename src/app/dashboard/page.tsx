a738623af1f34b1c9cc2302b31aad5ab20c1e08d
trueNebula <daniel.ggradinaru@gmail.com>
2024-10-25 15:48:25 +0300

    add temp check for user data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,22 +1,17 @@
 import { getAuthSession } from '@/server/auth';
-import Loader from '@/components/loader.tsx';
 import Navbar from '@/app/dashboard/_components/navbar/navbar';
 import DashboardContainer from '@/app/dashboard/_components/dashboard/dashboardContainer';
 
 export default async function Dashboard() {
   const session = await getAuthSession();
-  // console.log(data, status);
-
-  // if (status === 'loading' || !data?.user.name || !data?.user.image) {
-  //   return (
-  //     <div className="min-h-screen w-full flex justify-center items-center">
-  //       <Loader />
-  //     </div>
-  //   );
-  // }
 
   if (!session || !session.user) {
     return <div>Unauthorized</div>;
+  }
+
+  if (!session.user.name || !session.user.image) {
+    console.error('User data is missing');
+    return null;
   }
 
   const loggedUser = {
